0da91e592b9387419b5dcced8f4b330b52d926f2
shocik <shocik@gmail.com>
2021-02-06 12:56:11 +0100

    pretrain

--- models/vgg16_deconvnet.py
+++ models/vgg16_deconvnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+
 vgg16_pretrained = models.vgg16(pretrained=True)
 
 def double_conv(in_channels, out_channels):
@@ -43,20 +44,62 @@ def triple_deconv(in_channels, out_channels):
 class VggDeconvNet(nn.Module):
     def __init__(self, n_classes=2):
         super().__init__()
+        self.encoder = torch.nn.Sequential(
+            # conv1
+            torch.nn.Conv2d(3, 64, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, stride=2, return_indices=True),
+            # conv2
+            torch.nn.Conv2d(64, 128, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, 128, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, stride=2, return_indices=True),
+            # conv3
+            torch.nn.Conv2d(128, 256, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, 256, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, 256, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, stride=2, return_indices=True),
+            # conv4
+            torch.nn.Conv2d(256, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, stride=2, return_indices=True),
+            # conv5
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, stride=2, return_indices=True)
+        )
+
+        self.pool_indices = dict()
+
+        # initialize weights
+        for i in range(len(self.encoder)):
+            if isinstance(self.encoder[i], torch.nn.Conv2d):
+                self.encoder[i].weight.data = vgg16_pretrained.features[i].weight.data
+                self.encoder[i].bias.data = vgg16_pretrained.features[i].bias.data
         
-        self.conv1 = double_conv(3, 64)
-        self.conv2 = double_conv(64, 128)
-        self.conv3 = triple_conv(128, 256)
-        self.conv4 = triple_conv(256, 512)
-        self.conv5 = triple_conv(512, 512)
-        self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=True)
         self.bottom = nn.Sequential(
           nn.Conv2d(512, 512, 3, padding=1),
           nn.ReLU(inplace=True),
           nn.Conv2d(512, 512, 3, padding=1),
           nn.ReLU(inplace=True),
           nn.ConvTranspose2d(512, 512, 3, padding=1),
-          nn.ReLU(inplace=True))
+          nn.ReLU(inplace=True)
+        )
+
         self.deconv5 = triple_deconv(512,512)
         self.deconv4 = triple_deconv(512,256)
         self.deconv3 = triple_deconv(256,128)
@@ -73,32 +116,35 @@ class VggDeconvNet(nn.Module):
             torch.nn.Linear(4096, 1))
         
         #initialize weights with pretrained
-        
+    def forward_encoder(self, x):
+        output = x
+        i=1
+        for layer in self.encoder:
+            if isinstance(layer, torch.nn.MaxPool2d):
+                output, indices = layer(output)
+                #self.feature_outputs[i] = output
+                self.pool_indices[i] = indices
+            else:
+                output = layer(output)
+                #self.feature_outputs[i] = output
+        return output
+
     def forward(self, x):
-        x = self.conv1(x)
-        x, ind1 = self.maxpool(x)
-        x = self.conv2(x)
-        x, ind2 = self.maxpool(x)
-        x = self.conv3(x)
-        x, ind3 = self.maxpool(x)
-        x = self.conv4(x)
-        x, ind4 = self.maxpool(x)
-        x = self.conv5(x)
-        x, ind5 = self.maxpool(x)
+        x = self.forward_encoder(x)
         x = self.bottom(x)
         #x = x.view(x.size()[0], -1)
         #print(x.size())
         #x = self.classifier(x)
         #print(x.size())
-        x = self.maxunpool(x, ind5)
+        x = self.maxunpool(x, self.pool_indices[5])
         x = self.deconv5(x)
-        x = self.maxunpool(x, ind4)
+        x = self.maxunpool(x, self.pool_indices[4])
         x = self.deconv4(x)
-        x = self.maxunpool(x, ind3)
+        x = self.maxunpool(x, self.pool_indices[3])
         x = self.deconv3(x)
-        x = self.maxunpool(x, ind2)
+        x = self.maxunpool(x, self.pool_indices[2])
         x = self.deconv2(x)
-        x = self.maxunpool(x, ind1)
+        x = self.maxunpool(x, self.pool_indices[1])
         x = self.deconv1(x)
 
         return x  
